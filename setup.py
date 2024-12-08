d516fe639a38da0bb91b3d3270eebf8a0cc238b3
cappuch <mikusdevr@gmail.com>
2024-12-08 19:59:29 +0000

    refactor and custom training loops

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rndmcoolawsmgrbg/WIIIIIDE/tree/dev_package",
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
         "torch",
         "cryptography",
